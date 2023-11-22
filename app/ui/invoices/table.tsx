9e172af24ba3a095d54dd59f2a2e07b36f20a987
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2023-11-22 20:19:46 +0500

    Add files via upload

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -26,11 +26,12 @@ export default async function InvoicesTable({
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
-                      <Image
+                      <image
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
+                       alt="Image" 
                       />
                       <p>{invoice.name}</p>
                     </div>
@@ -89,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                                                alt='Image'                        alt='Image                        alt='Image'alt="Image"'
                       />
                       <p>{invoice.name}</p>
                     </div>
